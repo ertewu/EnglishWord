afbb96e10c4527ac8f7bef094146f759765613ba
wzy <ertewu@163.com>
2017-05-05 19:41:13 +0800

    继续修改

---
+++ src/main/java/zystudio/englishword/web/WebConfig.java
@@ -0,0 +1,13 @@
+package zystudio.englishword.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Created by wzy on 17-5-5.
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+}
