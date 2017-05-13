95c996421e22bd6199199a53df203a104f2ac8b2
ertewu <ertewu@gmail.com>
2017-05-13 23:59:01 +0800

    补全gradle的依赖和maven依赖,同时把project的java version 升级到8,现在总算配置的事全解决了，下边可以专注工程和代码上的事了

---
+++ src/main/java/zystudio/englishword/config/DataConfig.java
@@ -0,0 +1,10 @@
+package zystudio.englishword.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by wzy on 5/12/17.
+ */
+@Configuration
+public class DataConfig {
+}
